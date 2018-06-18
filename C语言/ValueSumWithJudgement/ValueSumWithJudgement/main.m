1e0a8b5015699b265ea72e32fa7f2e0baad83b50
yxy0720 <479397375@qq.com>
2018-06-18 22:27:13 +0800

    add value sum without jugement

---
+++ C语言/ValueSumWithJudgement/ValueSumWithJudgement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ValueSumWithJudgement
+//
+//  Created by lanjiying on 2018/6/18.
+//  Copyright © 2018年 lanjiying_Angelia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+    }
+    return 0;
+}
